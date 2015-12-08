ae05e3cbb5d68337b5712ae2852717c021e88601
JohnyWilson <imemyself9163@gmail.com>
2015-12-08 01:08:25 -0600

    Added WordCount

--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,14 +1,17 @@
 /**   * Created by urlama1 on 12/7/2015.    */
 
 import org.apache.spark.{SparkConf, SparkContext}
-
 object WordCount {
   def main (args: Array[String]) {
     //Create Spark Context
-    val conf = new SparkConf().setMaster("local").setAppName("MySpark_Test")
+    val conf = new SparkConf().setMaster("local").setAppName("WordCount")
     val sc = new SparkContext(conf)
-    val data = sc.parallelize(1 to 1000000).collect().filter(_<100)
-    data.foreach(println)
+    val WordsCounted = sc.textFile("Data/romeo.txt")
+                          .flatMap(_.split(" "))
+                          .map((_,1))
+                          .reduceByKey(_+_)
+                          .map{ case(a,b) => (b,a)}
+                          .sortByKey(ascending = false)
+    WordsCounted.foreach(println)
   }
-
 }
