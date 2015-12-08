555689c7e85b30dace02d3b62cb96d3bbe88e214
JohnyWilson <imemyself9163@gmail.com>
2015-12-07 19:08:17 -0600

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "TestSpark"
+
+version := "1.0"
+
+scalaVersion := "2.11.5"
+    
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.5.2"
+
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "1.5.2"
---
+++ src/main/scala/TestMain.scala
@@ -0,0 +1,13 @@
+/**   * Created by urlama1 on 12/7/2015.    */
+
+import org.apache.spark.{SparkContext, SparkConf}
+object TestMain {
+  def main (args: Array[String]) {
+    //Create Spark Context
+    val conf = new SparkConf().setMaster("local").setAppName("MySpark_Test")
+    val sc = new SparkContext(conf)
+    val data = sc.parallelize(1 to 1000000).collect().filter(_<100)
+    data.foreach(println)
+  }
+
+}
